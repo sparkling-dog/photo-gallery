a3374273b7bef4653a7298e0cc604ab1bcfa0c94
sepuckett86 <sepuckett86@gmail.com>
2019-05-20 16:10:37 -0700

    heck yeah! three points!! also, yay for learning.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import template from './template.js';
 import htmlToDOM from './html-to-DOM.js';
 import buildings from '../data/buildings.js';
+import templateHeader from './template-header.js';
 
 const gallery = document.getElementById('gallery');
+const header = document.getElementById('header');
 
 buildings.forEach(building => {
     const html = template(building);
     const dom = htmlToDOM(html);
     gallery.appendChild(dom);
 });
+
+const html = templateHeader();
+const dom = htmlToDOM(html);
+
+header.appendChild(dom);
--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,4 @@
 import './html-equal.js';
 
 import './template.test.js';
+import './template-header.test.js';
