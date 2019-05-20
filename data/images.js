7b1a6959b4cca5e0b054b61b0dc240503c0315ab
sepuckett86 <sepuckett86@gmail.com>
2019-05-20 14:58:14 -0700

    first commit with files built, added images, formatted image objects

--- data/images.js
+++ data/images.js
@@ -1,9 +1,21 @@
 const images = [
     {
-        url: '',
-        title: '',
-        description: '',
-        keyword: ''
+        url: '../assets/Acropolis.jpg',
+        title: 'Acropolis',
+        description: 'Ancient greek building from 19th century b.c.',
+        keyword: 'acropolis'
+    },
+    {
+        url: '../assets/Great-Temple-of-Amun.jpg',
+        title: 'Great-Temple-of-Amun',
+        description: 'Ancient Egyption building from 1530 b.c.',
+        keyword: 'amun'
+    },
+    {
+        url: '../assets/Machu-Picchu.jpg',
+        title: 'Machu-Picchu',
+        description: 'Ancient Peruvian building 15th century a.d.',
+        keyword: 'picchu'
     }
 ];
 
